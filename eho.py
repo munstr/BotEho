0f4f1049f7836e0b2ec1bfd8007b8a1ae9de0e01
munstr <svalera07@mail.ru>
2022-03-17 16:09:40 +0500

    Initial app commit

---
+++ eho.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+from flask import Flask, request
+
+bot = telebot.TeleBot(os.environ["token"])
+
+server = Flask(__name__)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
