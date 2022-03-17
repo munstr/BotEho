8197a25bbac062864f577864af0672a4d51f2905
munstr <svalera07@mail.ru>
2022-03-17 20:51:01 +0500

    Initial app commit

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 import telebot
 from flask import Flask, request
 
-bot = telebot.TeleBot(os.environ[token])
+token = '5254391359:AAHHzp56Sx79SP0q9MrsRc3klaXwUAk21Do'
+
+bot = telebot.TeleBot(token)
 
 server = Flask(__name__)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
+def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
